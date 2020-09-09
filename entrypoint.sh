0aa525fd4cdc26d69199629cc43fef62da22e235
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2020-09-09 13:53:58 +0200

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 
 wget "https://github.com/JakeWharton/diffuse/releases/download/$INPUT_VERSION/diffuse-$INPUT_VERSION-binary.jar" -q -O diffuse.jar
-chmod +x diffuse.jar
 
 args=()
 diff=$(java -jar diffuse.jar diff "${args[@]}" "$INPUT_FILE" "$INPUT_FILE")
@@ -9,4 +8,5 @@ diff=$(java -jar diffuse.jar info "${args[@]}" "$INPUT_FILE")
 diff="${diff//'%'/'%25'}"
 diff="${diff//$'\n'/'%0A'}"
 diff="${diff//$'\r'/'%0D'}"
+echo "Dupa $diff"
 echo "::set-output name=text-diff::$diff"
