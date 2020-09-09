cfab64ed2440d6b35211caef8abd727de7e407a4
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2020-09-09 13:47:26 +0200

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
-wget "https://github.com/JakeWharton/diffuse/releases/download/$INPUT_VERSION/diffuse-$INPUT_VERSION-binary.jar" -O diffuse
+wget "https://github.com/JakeWharton/diffuse/releases/download/$INPUT_VERSION/diffuse-$INPUT_VERSION-binary.jar" -O diffuse.jar
 ls -l
-chmod +x diffuse
+chmod +x diffuse.jar
 
 args=()
-diff=$(diffuse "${args[@]}" "$INPUT_FILE" "$INPUT_FILE")
+diff=$(diffuse.jar "${args[@]}" "$INPUT_FILE" "$INPUT_FILE")
 diff="${diff//'%'/'%25'}"
 diff="${diff//$'\n'/'%0A'}"
 diff="${diff//$'\r'/'%0D'}"
