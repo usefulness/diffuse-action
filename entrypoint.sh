f233b94971eea4d21e7995070e54f67d982fe9f8
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2020-09-09 13:41:51 +0200

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 wget "https://github.com/JakeWharton/diffuse/releases/download/$INPUT_VERSION/diffuse-$INPUT_VERSION-binary.jar" -q -O diffuse.jar
 chmod +x diffuse.jar
 
-diff=$(java -jar diffuse.jar --jar "$INPUT_FILE" "$INPUT_FILE")
+diff=$(diffuse.jar "$INPUT_FILE" "$INPUT_FILE")
 diff="${diff//'%'/'%25'}"
 diff="${diff//$'\n'/'%0A'}"
 diff="${diff//$'\r'/'%0D'}"
