12348ec2a36ad6f3bdb97f634bcf1ec5b40eae73
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2020-09-13 22:11:57 +0200

    Switch to python

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-downloadArgs=()
-[ "$INPUT_DEBUG" != true ] && downloadArgs+=(-q)
-
-wget "https://github.com/JakeWharton/diffuse/releases/download/$INPUT_VERSION/diffuse-$INPUT_VERSION-binary.jar" "${downloadArgs[@]}" -O diffuse.jar
-
-[ "$INPUT_DEBUG" == true ] && ls -l
-python3 parser.py
